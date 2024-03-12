4a3f2628fa10fb59895b4cb27613ccee43c951cd
Jeon <sanggyu1008@naver.com>
2024-03-12 20:05:42 +0900

    homework

---
+++ Homework240312_202013297.py
@@ -0,0 +1,87 @@
+# 해양학전공 전상규 
+
+# 2.1
+print( 100,'+', 200 ,'=', 100 + 200 )
+print( 200,'+', 300 ,'+', 400 ,'=', 200 + 300 + 400 )
+
+# 2.2
+width = 30
+height = 60
+print(width)
+print(height)
+
+# 2.3
+area = str(width * height)
+print('사각형의 면적 : ' + area)
+
+# 2.5
+length = int(input('정사각형의 밑변을 입력하시오 : '))
+area2 = str(length**2)
+print('정사각형의 면적 : ' + area2)
+
+# 2.7
+x = str(1*2*3*4*5*6*7*8*9*10)
+print('10! = '+ x)
+
+# 2.9
+celsius = [' 0', 10, 20, 30, 40, 50]
+fahrenheit = []
+
+for i in range(6):
+    fahrenheit.append(9/5 * float(celsius[i]) + 32)
+
+print('섭씨'+'     '+'화씨')
+for i in range(6):
+    print(str(celsius[i])+'       '+str(fahrenheit[i]))
+
+# 2.11
+fahrenheit = str(input('화씨 온도를 입력하세요 : '))
+celsius = str(9/5 * int(fahrenheit) + 32)
+
+print('화씨 '+fahrenheit+' 도는 섭씨 '+celsius+' 도 입니다.')
+
+# 2.13
+PI = 3.141592
+r = input('원의 반지름을 입력하세요 : ')
+원의둘레 = str(2 * PI * float(r))
+원의면적 = str(PI * float(r)**2)
+
+print('원의 둘레 = '+원의둘레+', 원의 면적 = '+원의면적)
+
+# 2.14
+for i in range(2,11,1):
+    print(str(i)+'의 제곱근 = '+str(i**(1/2)))
+
+# 2.15
+a = input('밑변의 길이를 입력하세요.')
+b = input('높이를 입력하세요.')
+c = int(a)**2 + int(b)**2
+
+print('빗변의 길이는 '+str(c**(1/2))+' 입니다.')
+
+# 2.17
+print(2 << 0, 2 << 1, 2 << 2, 2 << 3, 2 << 4, 2 << 5, 2 << 6, 2 << 7, 2 << 8, 2 << 9)
+
+# 2.19
+정수 = int(input('정수를 입력하세요 : '))
+
+if 정수 % 2 == 1 and 0 <= 정수 <= 100 :
+    print('입력된 정수는 0에서 100의 범위 안에 있는 짝수인가요? False')
+elif 정수 % 2 ==0 and 0 <= 정수 <= 100 :
+    print('입력된 정수는 0에서 100의 범위 안에 있는 짝수인가요? True')
+else:
+    print('입력된 정수가 0에서 100의 범위 안에 있지 않습니다.')
+    
+# 2.21
+정수 = int(input('정수를 입력하시오 : '))
+
+print(str(정수)+' 의 2진수 값 : '+bin(정수))
+print(str(정수)+' 의 2진수 값에 대한 비트단위 부정값 :'+bin(~정수))
+
+# 2.23
+n = int(input('세 자리 정수를 입력하시오 : '))
+
+print("백의 자리 : "+ str(n // 100))
+print("십의 자리 : "+ str((n // 10)%10))
+print("일의 자리 : "+ str(n % 10))
+
---
+++ test_240311.py
@@ -0,0 +1,51 @@
+# 2024/03/11 AI프로그래밍및실습 해양학전공 전상규
+
+"""
+input = 4.0     # 변수 이름을 내장함수와 같게 설정
+#print("input", input)
+#name = input("이름은?")     # input이 내장함수가 아닌 변수로 작용
+"""
+
+"""
+age = int(input("What is ur age ?"))        # VScode에서는 input()함수를 사용할때는 실행 및 디버그 사용
+print(type(age))
+"""
+
+"""
+a = 1
+for i in range(1, 11, 1):
+    a *= i
+    print(a)
+print(a)
+"""
+
+""" 주석을 달 때 코드를 누가, 언제, 무엇을 위해서 작성하였는지 기록 """
+
+"""
+이렇게 긴 주석을 달 때는 
+따움표를 3개 단다
+"""
+
+"""
+age = int(input("나이를 입력하세요"))
+
+if age < 20:
+    print("청소년 할인 대상입니다.")
+else:
+    print("나이가 너무 많아요ㅠㅠ")
+"""
+
+"""
+month = int(input("오늘은 몇 월 입니까? (숫자만 입력하세요)"))
+
+if month <= 2:
+    print(str(month) & "은 겨울입니다")
+elif month <=5:
+    print(month)
+elif month <=8:
+    print(month)
+elif month <=11:
+    print(month)
+else:
+    print(month)
+"""
